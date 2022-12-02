4f2d7f4901b528d510f853884c9d943ba609ff35
ValeriaStratonova <stratonova.valeria02@yandex.ru>
2022-12-02 10:33:25 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Конвертер_курса_валют
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.money = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(79, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(67, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(79, 97);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(67, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(152, 98);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Font = new System.Drawing.Font("Papyrus", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(28, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(270, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Конвертировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // money
+            // 
+            this.money.Location = new System.Drawing.Point(152, 43);
+            this.money.Name = "money";
+            this.money.Size = new System.Drawing.Size(130, 20);
+            this.money.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(45, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(28, 21);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(45, 97);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(28, 21);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Papyrus", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 21);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Papyrus", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 21);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(326, 207);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.money);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximumSize = new System.Drawing.Size(342, 246);
+            this.MinimumSize = new System.Drawing.Size(342, 246);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Конвертер валют";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox money;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Конвертер_курса_валют
+{
+    public partial class Form1 : Form
+    {
+        SQLiteConnection connection = null;
+        SQLiteDataAdapter adapter = null;
+        string currency1 = null;
+        string currency2 = null;
+        string file = null;
+        DataTable dtable = null;
+        DataTable pic = null;
+        string[] tables = null;
+        string n1;
+        string n2;
+        string x;
+        string y;
+        public Form1()
+        {
+            InitializeComponent();
+            file = @"D:\\Study\\Сопровождение ПО\\Валюты.db";
+            connection = new SQLiteConnection("Data Source=" + file);
+            connection.Open();
+            adapter = new SQLiteDataAdapter("SELECT Валюта FROM Валюты", connection);
+            dtable = new DataTable();
+            adapter.Fill(dtable);
+            tables = new string[dtable.Rows.Count];
+
+            for (int i = 0; i < tables.Length; i++)
+            {
+                tables[i] = dtable.Rows[i].ItemArray[0].ToString();
+            }
+            comboBox1.Items.AddRange(tables);
+            comboBox2.Items.AddRange(tables);
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+
+            label1.Visible = false;
+            label2.Visible = false;
+            label1.BackColor = Color.Transparent;
+            label2.BackColor = Color.Transparent;
+            label1.ForeColor = Color.White;
+            label2.ForeColor = Color.White;
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox2.BackColor = Color.Transparent;
+            textBox1.ReadOnly = true;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            currency1 = comboBox1.SelectedItem.ToString();
+            adapter = new SQLiteDataAdapter($"SELECT Флаг FROM Валюты where Валюта = '{currency1}'", connection);
+            pic = new DataTable();
+            adapter.Fill(pic);
+            var im = new MemoryStream((byte[])pic.Rows[0].ItemArray[0]);
+            pictureBox1.Image = Image.FromStream(im);
+            adapter = new SQLiteDataAdapter($"SELECT Название FROM Валюты where Валюта = '{currency1}'", connection);
+            dtable = new DataTable();
+            adapter.Fill(dtable);
+            label1.Visible = true;
+            label1.Text = Convert.ToString(dtable.Rows[0].ItemArray[0]);
+            button1.Enabled = true;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            currency2 = comboBox2.SelectedItem.ToString();
+            adapter = new SQLiteDataAdapter($"SELECT Флаг FROM Валюты where Валюта = '{currency2}'", connection);
+            pic = new DataTable();
+            adapter.Fill(pic);
+            var im = new MemoryStream((byte[])pic.Rows[0].ItemArray[0]);
+            pictureBox2.Image = Image.FromStream(im);
+            adapter = new SQLiteDataAdapter($"SELECT Название FROM Валюты where Валюта = '{currency2}'", connection);
+            dtable = new DataTable();
+            adapter.Fill(dtable);
+            label2.Visible = true;
+            label2.Text = Convert.ToString(dtable.Rows[0].ItemArray[0]);
+            button1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (comboBox1.SelectedIndex == -1 || comboBox2.SelectedIndex == -1)
+            {
+                MessageBox.Show(
+                           "Валюты не выбраны",
+                           "Информация",
+                           MessageBoxButtons.OKCancel,
+                           MessageBoxIcon.Information);
+                money.Text = "";
+            }
+
+            else
+            {
+                if (comboBox1.SelectedIndex == comboBox2.SelectedIndex)
+                {
+                    MessageBox.Show(
+                       "Выбрана одна и та же валюта",
+                       "Информация",
+                       MessageBoxButtons.OKCancel,
+                       MessageBoxIcon.Information);
+
+                    money.Text = "";
+                    textBox1.Text = "";
+                    button1.Enabled = false;
+                }
+
+                else
+                {
+                    if (money.Text == "")
+                    {
+                        MessageBox.Show(
+                           "Вы не ввели конвертируемое число",
+                           "Информация",
+                           MessageBoxButtons.OKCancel,
+                           MessageBoxIcon.Information);
+                    }
+
+                    else
+                    {
+                        try
+                        {
+                            Convert.ToInt32(money.Text);
+                            double value = Convert.ToDouble(money.Text);
+
+                            if (value <=0)
+                            {
+                                MessageBox.Show(
+                                        "Нельзя конвертировать отрицательное число",
+                                        "Информация",
+                                        MessageBoxButtons.OKCancel,
+                                        MessageBoxIcon.Information);
+                                money.Text = "";
+                                textBox1.Text = "";
+                            }
+
+                            else
+                            {
+                                var url = "http://www.cbr.ru/scripts/XML_daily.asp";
+
+                                var request = WebRequest.Create(url);
+                                request.Method = "GET";
+
+                                var webResponse = request.GetResponse();
+                                var webStream = webResponse.GetResponseStream();
+
+                                var reader = new StreamReader(webStream, Encoding.Default);
+                                var data = reader.ReadToEnd();
+
+                                XElement source = XElement.Parse(data);
+                                var valutes = source.Descendants("Valute");
+
+                                foreach (XElement valute in valutes)
+                                {
+                                    if (currency1 == "RUB")
+                                    {
+                                        x = Convert.ToString(1);
+                                        n1 = Convert.ToString(1);
+
+                                    }
+                                    if (currency2 == "RUB")
+                                    {
+                                        y = Convert.ToString(1);
+                                        n2 = Convert.ToString(1);
+
+                                    }
+                                    if (valute.Element("CharCode").Value == currency1)
+                                    {
+                                        x = (string)valute.Element("Value");
+                                        n1 = (string)valute.Element("Nominal");
+                                    }
+                                    if (valute.Element("CharCode").Value == currency2)
+                                    {
+
+                                        y = (string)valute.Element("Value");
+                                        n2 = (string)valute.Element("Nominal");
+                                    }
+
+
+                                }
+
+                                double z = ((value * Convert.ToDouble(x)) / (Convert.ToDouble(y) * Convert.ToDouble(n1))) * Convert.ToDouble(n2);
+                                textBox1.Text = Math.Round(z, 4).ToString();
+                            }
+                        }
+
+                            
+                        catch (FormatException)
+                        {
+                            MessageBox.Show("Введено не число!",
+                                "Информация",
+                               MessageBoxButtons.OKCancel,
+                               MessageBoxIcon.Information);
+                            money.Text = "";
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
